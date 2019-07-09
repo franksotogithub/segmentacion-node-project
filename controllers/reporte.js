4a8c96d7a5f4d7998fb2a213387280131b2b4b53
franksotogithub <franksoto2012@gmail.com>
2019-07-09 10:15:09 -0500

    inicio de servicios de calidad

--- controllers/reporte.js
+++ controllers/reporte.js
@@ -219,9 +219,136 @@ let Reporte = {
 
 
 
-    }
+    },
+
+    reporte_avance_calidad(req,res){
+
+        let ambito=!(req.params.ambito)?0:req.params.ambito;
+        let codigo=!(req.params.codigo)?'00':req.params.codigo;
+        let amb=ambitos.find(x=>x.ambito==ambito);
+        let match={};
+
+        if(codigo!=='00'){
+            (codigo.length==2)? match['ccdd']= {'$in' : [codigo]}:(codigo.length==4)? match['ccpp']= {'$in' : [codigo]}:(codigo.length==6)? match['ccdi']= {'$in' : [codigo]}:true;
+        }
 
 
+        let groupBy={
+            _id:{'descripcion':'$'+amb.descripcion ,"codigo":'$'+amb.codigo },
+            "cant_zona_marco":{"$sum": 1 },
+            "cant_zona_segm":{"$sum": "$flag_proc_segm" }
+        }
+        let sort={}
+        sort[amb.codigo]=-1;
+
+        zonaModel.aggregate([
+            {"$match":match},
+
+
+            {"$group" : groupBy},
+            { "$project": {
+                    "codigo":'$_id.codigo',
+                    "descripcion":'$_id.descripcion',
+                    "cant_zona_marco": 1,
+                    "cant_zona_segm": 1,
+
+                    "porcent_segm":
+                        { "$multiply": [ { "$divide": [ "$cant_zona_segm", "$cant_zona_marco"] }, 100 ] }
+
+                }
+            }, { "$sort" :  {'codigo':1}}
+
+        ]
+            ,(err,data)=>{
+                console.log('err>>>',err);
+                console.log('data>>>',data);
+                if(err) res.status(500).json({message:"Error al recuperar data",error:err});
+
+
+
+                zonaModel.aggregate(
+                    [
+                                {"$match":match},
+                                {"$group":{_id:null,
+                                        "cant_ae_u":{"$sum":"$cant_ae_u"},
+                                        "cant_secc_u":{"$sum":"$cant_secc_u"},
+                                        "cant_mzs":{"$sum":"$cant_mzs"},
+                                        "cant_viv":{"$sum":"$cant_viv"},
+                                        "cant_pob":{"$sum":"$cant_pob"},
+
+                                }},
+
+                    ] ,
+                    (err,data1)=>{
+                    if(err) res.status(500).json({message:"Error al recuperar data",error:err});
+
+                    let promedios=[];
+                    let informativa=[];
+                    console.log(data1);
+
+                     let cant_aeus=data1[0].cant_ae_u;
+                    if(data1.length>0 && cant_aeus>0){
+                        let el={};
+
+                        el.label='Cantidad de secciones urbanas';
+                        el.valor= data1[0].cant_secc_u;
+                        informativa.push(el);
+
+                        el={};
+                        el.label='Cantidad de areas de empadronamiento urbano';
+                        el.valor= cant_aeus;
+                        informativa.push(el);
+                        el={};
+                        el.label='Promedio de manzanas por area de empadronamiento';
+                        el.valor= data1[0].cant_mzs/ cant_aeus;
+                        promedios.push(el);
+
+                        el={};
+                        el.label='Promedio de viviendas por area de empadronamiento';
+                        el.valor= data1[0].cant_viv/ cant_aeus;
+                        promedios.push(el);
+                        el={};
+                        el.label='Promedio de Poblacion por area de empadronamiento';
+                        el.valor= data1[0].cant_pob/cant_aeus;
+                        promedios.push(el);
+
+                    }
+
+                    aeuModel.aggregate(
+                        [
+                            {"$match":match},
+                            {$group:{ _id:{"cant_viv":"$cant_viv" } ,"valor":{"$sum":1} }},
+                            { "$sort" :  {'_id.cant_viv':1}}
+
+                        ],
+
+                        (err,data2)=>{
+                            if(err) res.status(500).json({message:"Error al recuperar data",error:err});
+                            let viviendas=[];
+                            let viviendasGraficos=[];
+
+                            console.log('cant_aeus>>',cant_aeus);
+                            data2.forEach( (data,index )=>{
+                                let el={},el2={};
+                                el.label=`Area de empadronamiento con  ${data._id.cant_viv} viviendas` ;
+                                el.valor= data.valor;
+                                el.porcent= data.valor/cant_aeus*100;
+                                el2=Object.assign({}, el);
+                                el2.label=`${data._id.cant_viv} viv`; 
+                                
+                                viviendas.push(el);
+                                viviendasGraficos.push(el2);
+                            });
+                            res.json({reporte:data, estadisticas:{viviendas:viviendas,promedios:promedios ,informativa:informativa} , graficos:{grafico1:viviendasGraficos}});
+                    });
+
+                    });
+
+        });
+
+    },
+
+    
 }
 
 module.exports = Reporte;
